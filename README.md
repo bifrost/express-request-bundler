28544e580d70cb7076338b5442741b702001c35f
Jonatan Pedersen <jp@jonatanpedersen.com>
2015-02-25 15:04:48 +0100

    initial

---
+++ README.md
@@ -0,0 +1,38 @@
+# Express Request Bundler
+
+## Install
+
+```bash
+$ npm install express-request-bundler --save
+```
+
+## Usage
+
+_Notice_ that express-ab requires the [cookie-parser](https://www.npmjs.org/package/cookie-parser) middleware to remember which variant the user was served.
+
+```javascript
+
+var app = require('express')();
+var erb = require('./src/express-request-bundler');
+
+app.get('/api/users/:id', function (req, res, next) {
+  db.users(req.params.id).then(user) {
+    res.json(user);
+  }
+});
+
+app.get('/api/users', function (req, res, next) {
+  db.users().then(users) {
+    res.json(users);
+  }
+});
+
+app.get('/api/bundle', erb());
+
+var server = app.listen(3000);
+
+app.listen(8080)
+```
+
+## Licence
+The MIT License (MIT)
---
+++ src/express-request-bundler.js
@@ -0,0 +1,104 @@
+/*
+	Express middleware for bundling requests in Express.
+*/
+
+var http = require('http');
+var url = require('url');
+
+function getParameters(query) {
+	var parameters = Object.keys(query).map(function(key) {
+		return {
+			name: key,
+			value: query[key]
+		};
+	});
+
+	return parameters;
+}
+
+function createOptions(originalRequest, newPath) {
+	var originalRequestUrl = url.parse(originalRequest.protocol + '://' + originalRequest.headers.host + originalRequest.url);
+
+	var options = {
+		method: 'GET',
+		path: newPath,
+		hostname: originalRequestUrl.hostname,
+		port: originalRequestUrl.port,
+		headers: originalRequest.headers
+	}
+
+	return options;
+}
+
+function createPromise(options, output, key) {
+	var promise = new Promise(function (resolve, reject) {
+		var request = http.request(options, function(response) {
+			var statusCode = response.statusCode;
+
+			output.write('"' + key + '": {');
+			output.write('"statusCode":' + statusCode + ',');
+			output.write('"headers":' + JSON.stringify(response.headers));
+			 
+			if (statusCode === 200)
+				output.write(',"body":');
+
+	      	response
+	      		.on('data', function(data) {
+	      			if (statusCode === 200)
+						output.write(data);		
+	      		})
+				.on('end', function() {
+					output.write('},');
+
+					resolve(statusCode);	
+	      		})	
+				.on('error', function() {
+					resolve(statusCode);
+	      		});	
+		});
+
+
+		request.end();
+	});
+
+	return promise;
+};
+
+function errors(statusCodes)  {
+	return statusCodes.filter(function(statusCode) { 
+		console.log(statusCode);
+		return statusCode !== 200; 
+	}) > 0;
+}
+
+var create = function() {
+	return function(request, response, next) {
+		response.setHeader('Content-Type', 'application/json');
+		response.write('{');
+
+		var query = request.query;
+		var parameters = getParameters(query);
+
+		var promises = parameters
+			.map(function(parameter) {
+				var path = '/' + parameter.value;
+				var key = parameter.name;
+				var options = createOptions(request, path);
+				var promise = createPromise(options, response, key);
+
+				return promise;
+			});
+
+		Promise.all(promises)
+			.then(errors)
+			.then(function(errors) {
+				response.write('"errors":' + errors);
+				response.write('}');
+				response.end();
+
+				next();
+			});
+	}
+};
+
+module.exports = create;
